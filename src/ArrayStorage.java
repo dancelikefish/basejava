c5c28a002fa088786686ea94ca8ca83b85229d8f
dancelikefish <gr.golovenko@gmail.com>
2018-08-24 00:11:32 +0300

    methods size() and save() were edited

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,24 +5,21 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
+
     void clear() {
-        Arrays.fill(storage, 0, size(), null); // null in depends of a size()
+        Arrays.fill(storage, 0, size, null); // null in depends of a size()
     }
 
     void save(Resume r) {
-        boolean isEquals = false;
-        for (int i = 0; i < size(); i++) {
-            if (storage[i].uuid.equals(r.uuid)) { //saving in depends of size() func without nulls and duplicates
-                isEquals = true;
-            }
-        }
-        if (!isEquals) {
-            storage[size()] = r;
+        if (size < storage.length) {
+            storage[size] = r;
+            size++;
         }
     }
 
     Resume get(String uuid) {
-            for (int i = 0; i < size(); i++) {
+            for (int i = 0; i < size; i++) {
                 if (storage[i].uuid.equals(uuid)) { //searching id and returning at once
                     return storage[i];
                 }
@@ -31,9 +28,10 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                System.arraycopy(storage,i + 1, storage, i , size()); //edited to System.Arraycopy
+                System.arraycopy(storage,i + 1, storage, i , size); //edited to System.Arraycopy
+                size--;
                 break;
             }
         }
@@ -43,16 +41,6 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
-    }
-
-    int size() {
-        int sizeCounter = 0;
-        for (Resume resume : storage) {
-            if (resume != null ) {  // finding not null values, and setting size
-                sizeCounter++;
-            }
-        }
-        return sizeCounter;
+        return Arrays.copyOf(storage, size);
     }
 }
