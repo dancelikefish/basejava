73074520b05353e70e670820c760dedea6eecd88
dancelikefish <gr.golovenko@gmail.com>
2018-08-27 15:29:06 +0300

    - method isFound() & searchingIndex value are added - check to not null uuid is added - extra for-cycles are removed - MainArray soutUi is corrected

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     int size = 0;
+    int searchingIndex = 0;
 
     void clear() {
         Arrays.fill(storage, 0, size, null);
@@ -13,60 +14,50 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        boolean isEquals = false;
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(r.uuid) && size < storage.length) {
-                isEquals = true;
-            }
-        }
-        if (!isEquals) {
+        if (!isFound(r.uuid) && size < storage.length && r.uuid != null) {
             storage[size] = r;
             size++;
         } else {
-            System.out.println("Resume isn't unique or storage is overflowed ");
+            System.out.println("Resume isn't unique, or storage is overflowed, or uuid = null");
         }
     }
 
     void update(Resume r) {
-        int indexForUpdate = -1;
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(r.uuid)) {
-                indexForUpdate = i;
-            } else if (size - 1 == i && indexForUpdate == -1) {
-                System.out.println("Resume doesn't exist");
-                break;
-            }
-        }
-        if (indexForUpdate != -1) {
-            storage[indexForUpdate].uuid = r.uuid;
+        if (isFound(r.uuid)) {
+            storage[searchingIndex].uuid = r.uuid;
+        } else {
+            System.out.println("Resume doesn't exist");
         }
     }
 
     Resume get(String uuid) {
-        boolean isFound = false;
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) { //searching id and returning at once
-                isFound = true;
-                return storage[i];
-            } else if (i == size - 1 && !isFound) {
-                System.out.println("Resume doesn't exist");
-            }
+        if (isFound(uuid)) {
+            return storage[searchingIndex];
+        } else {
+            System.out.println("Resume doesn't exist");
         }
         return null;
     }
 
     void delete(String uuid) {
-        boolean isExist = false;
+        if (isFound(uuid)) {
+            storage[searchingIndex] = storage[searchingIndex + 1];
+            storage[searchingIndex + 1] = null;
+            size--;
+        } else {
+            System.out.println("Resume doesn't exist");
+        }
+    }
+
+    boolean isFound(String uuid) {
+        boolean isFound = false;
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                System.arraycopy(storage, i + 1, storage, i, size);
-                size--;
-                isExist = true;
-                break;
-            } else if (i == size - 1 && !isExist) {
-                System.out.println("Resume doesn't exist");
+                isFound = true;
+                searchingIndex = i;
             }
         }
+        return isFound;
     }
 
     /**
