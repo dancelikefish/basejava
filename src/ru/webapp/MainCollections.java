7505d3f294d03af4b1e9b36884e971e9356ec7c9
dancelikefish <gr.golovenko@gmail.com>
2018-10-16 17:55:54 +0300

    small fixes

--- src/ru/webapp/MainCollections.java
+++ src/ru/webapp/MainCollections.java
@@ -6,12 +6,15 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class MainCollections {
+    private static final Resume r1 = new Resume("uuid1");
+    private static final Resume r2 = new Resume("uuid2");
+    private static final Resume r3 = new Resume("uuid3");
+
     public static void main(String[] args) {
         Collection<Resume> collection = new ArrayList<>();
 
-        Resume r1 = new Resume("uuid1");
-        Resume r2 = new Resume("uuid2");
-        Resume r3 = new Resume("uuid3");
-
+        collection.add(r1);
+        collection.add(r2);
+        collection.add(r3);
     }
 }
