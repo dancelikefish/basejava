8b406f373c09a9a3f90950f369d69fb2eaeff119
dancelikefish <gr.golovenko@gmail.com>
2018-10-17 03:06:14 +0300

    introducing MainCollections class

--- src/ru/webapp/MainCollections.java
+++ src/ru/webapp/MainCollections.java
@@ -4,6 +4,8 @@ import ru.webapp.model.Resume;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.*;
+import java.util.Iterator;
 
 public class MainCollections {
     private static final Resume r1 = new Resume("uuid1");
@@ -16,5 +18,26 @@ public class MainCollections {
         collection.add(r1);
         collection.add(r2);
         collection.add(r3);
+
+        Iterator<Resume> iterator = collection.iterator();
+
+        while (iterator.hasNext()) {
+            Resume r = iterator.next();
+            System.out.println(r);
+            if (r.getUuid().equals(r1.getUuid())) {
+                iterator.remove();
+            }
+        }
+        System.out.println(collection.toString());
+
+
+        HashMap<String, Resume> map = new HashMap<>();
+        map.put("uuid1", r1);
+        map.put("uuid2", r2);
+        map.put("uuid3", r3);
+
+        for (Map.Entry<String, Resume> entry : map.entrySet()) {
+            System.out.println(entry.getValue());
+        }
     }
 }
