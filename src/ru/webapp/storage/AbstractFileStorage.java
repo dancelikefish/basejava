d32b3e66265d86baf26ab2fa8abe82dc51c2955e
dancelikefish <gr.golovenko@gmail.com>
2018-11-30 00:34:31 +0300

    HW_08, AbstractFileStorage introducing

---
+++ src/ru/webapp/storage/AbstractFileStorage.java
@@ -0,0 +1,76 @@
+package ru.webapp.storage;
+
+import ru.webapp.exception.StorageException;
+import ru.webapp.model.Resume;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+public abstract class AbstractFileStorage extends AbstractStorage<File> {
+    private File directory;
+
+    protected AbstractFileStorage(File directory) {
+        Objects.requireNonNull(directory, "directory mustn't be null");
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not directory");
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not readable/writable");
+        }
+        this.directory = directory;
+    }
+
+    @Override
+    protected void saveInStorage(Resume resume, File file) {
+        try {
+            file.createNewFile();
+            doWrite(resume, file);
+        } catch (IOException e) {
+            throw new StorageException("IO error", file.getName(), e);
+        }
+    }
+
+    protected abstract void doWrite(Resume resume, File file) throws IOException;
+
+    @Override
+    protected void updateInStorage(Resume resume, File file) {
+
+    }
+
+    @Override
+    protected Resume getFromStorage(File file) {
+        return null;
+    }
+
+    @Override
+    protected void deleteFromStorage(File file) {
+
+    }
+
+    @Override
+    protected boolean isValid(File file) {
+        return file.exists();
+    }
+
+    @Override
+    protected File getSearchKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override
+    protected List<Resume> getCopyList() {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
