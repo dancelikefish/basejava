de9c7dac417b4e73b7bac7f1b12c826ae3b196f9
dancelikefish <gr.golovenko@gmail.com>
2018-11-29 18:01:58 +0300

    HW_08, fix ResumeTestData add DateUtil

--- src/ru/webapp/ResumeTestData.java
+++ src/ru/webapp/ResumeTestData.java
@@ -53,44 +53,44 @@ public class ResumeTestData {
         qualifications.add("Родной русский, английский \"upper intermediate\"");
         Section qualification = new ListSection(qualifications);
 
-        Organization javaOnlineProjects = new Organization("Java Online Projects", "Автор проекта.", "Создание, организация и " +
+        Organization javaOnlineProjects = new Organization("Java Online Projects","", "Автор проекта.", "Создание, организация и " +
                 "проведение Java онлайн проектов и стажировок", LocalDate.of(2013, 10, 1), LocalDate.now());
-        Organization wrike = new Organization("Wrike", "Старший разработчик (backend)", "Проектирование и разработка " +
+        Organization wrike = new Organization("Wrike","", "Старший разработчик (backend)", "Проектирование и разработка " +
                 "онлайн платформы управления проектами Wrike (Java 8 API, Maven, Spring, MyBatis, Guava, Vaadin, PostgreSQL, Redis). Двухфакторная аутентификация, " +
                 "авторизация по OAuth1, OAuth2, JWT SSO.", LocalDate.of(2014, 10, 1), LocalDate.of(2016, 1, 1));
-        Organization ritCenter = new Organization("RIT Center", "Java архитектор", "Организация процесса разработки системы ERP " +
+        Organization ritCenter = new Organization("RIT Center","", "Java архитектор", "Организация процесса разработки системы ERP " +
                 "для разных окружений: релизная политика, версионирование, ведение CI (Jenkins), миграция базы (кастомизация Flyway), конфигурирование системы (pgBoucer, Nginx), AAA " +
                 "via SSO. Архитектура БД и серверной части системы. Разработка интергационных сервисов: CMIS, BPMN2, 1C (WebServices), сервисов общего назначения (почта, экспорт в pdf, " +
                 "doc, html). Интеграция Alfresco JLAN для online редактирование из браузера документов MS Office. Maven + plugin development, Ant, Apache Commons, Spring security, " +
                 "Spring MVC, Tomcat,WSO2, xcmis, OpenCmis, Bonita, Python scripting, Unix shell remote scripting via ssh tunnels, PL/Python", LocalDate.of(2012, 4, 1),
                 LocalDate.of(2014, 10, 1));
-        Organization luxoft = new Organization("Luxoft (Deutsche Bank)", "Ведущий программист", "Участие в проекте Deutsche Bank" +
+        Organization luxoft = new Organization("Luxoft (Deutsche Bank)","", "Ведущий программист", "Участие в проекте Deutsche Bank" +
                 " CRM (WebLogic, Hibernate, Spring, Spring MVC, SmartGWT, GWT, Jasper, Oracle). Реализация клиентской и серверной части CRM. Реализация RIA-приложения для администрирования, " +
                 "мониторинга и анализа результатов в области алгоритмического трейдинга. JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Highstock, Commet, HTML5.", LocalDate.of(2010, 12, 1),
                 LocalDate.of(2012, 4, 1));
-        Organization yota = new Organization("YOTA", "Ведущий специалист", "Дизайн и имплементация Java EE фреймворка для отдела \"" +
+        Organization yota = new Organization("YOTA","", "Ведущий специалист", "Дизайн и имплементация Java EE фреймворка для отдела \"" +
                 "Платежные Системы\" (GlassFish v2.1, v3, OC4J, EJB3, JAX-WS RI 2.1, Servlet 2.4, JSP, JMX, JMS, Maven2). Реализация администрирования, статистики и мониторинга фреймворка." +
                 " Разработка online JMX клиента (Python/ Jython, Django, ExtJS)", LocalDate.of(2008, 6, 1), LocalDate.of(2010, 12, 1));
-        Organization enkata = new Organization("Enkata", "Разрабочик ПО", "Разработка информационной модели, проектирование интерфейсов," +
+        Organization enkata = new Organization("Enkata","", "Разрабочик ПО", "Разработка информационной модели, проектирование интерфейсов," +
                 " реализация и отладка ПО на мобильной IN платформе Siemens @vantage (Java, Unix).", LocalDate.of(2005, 1, 1), LocalDate.of(2007, 2, 1));
-        Organization alcatel = new Organization("Alcatel", "Инженер по аппаратному и программному тестированию", "Тестирование, отладка, внедрение " +
+        Organization alcatel = new Organization("Alcatel","", "Инженер по аппаратному и программному тестированию", "Тестирование, отладка, внедрение " +
                 "ПО цифровой телефонной станции Alcatel 1000 S12 (CHILL, ASM).", LocalDate.of(1997, 9, 1), LocalDate.of(2005, 1, 1));
         add(javaOnlineProjects, wrike, ritCenter, luxoft, yota, enkata, alcatel, occupationPlaces);
         Section experience = new OrganisationSection(occupationPlaces);
 
-        Organization coursera = new Organization("Coursera", "\"Functional Programming Principles in Scala\" by Martin Odersky", "",
+        Organization coursera = new Organization("Coursera","", "\"Functional Programming Principles in Scala\" by Martin Odersky", "",
                 LocalDate.of(2013, 3, 1), LocalDate.of(2013, 5, 1));
-        Organization luxofEdu = new Organization("Luxoft", "Курс \"Объектно-ориентированный анализ ИС. Концептуальное моделирование на UML.\"", "",
+        Organization luxofEdu = new Organization("Luxoft","", "Курс \"Объектно-ориентированный анализ ИС. Концептуальное моделирование на UML.\"", "",
                 LocalDate.of(2011, 3, 1), LocalDate.of(2011, 4, 1));
-        Organization siemensAg = new Organization("Siemens AG", "3 месяца обучения мобильным IN сетям (Берлин)", "",
+        Organization siemensAg = new Organization("Siemens AG","", "3 месяца обучения мобильным IN сетям (Берлин)", "",
                 LocalDate.of(2005, 1, 1), LocalDate.of(2005, 4, 1));
-        Organization alcatelEdu = new Organization("Alcatel", "6 месяцев обучения цифровым телефонным сетям (Москва)", "",
+        Organization alcatelEdu = new Organization("Alcatel","", "6 месяцев обучения цифровым телефонным сетям (Москва)", "",
                 LocalDate.of(1997, 9, 1), LocalDate.of(1998, 3, 1));
-        Organization spbNii = new Organization("Санкт-Петербургский национальный исследовательский университет информационных технологий, механики и оптики",
+        Organization spbNii = new Organization("Санкт-Петербургский национальный исследовательский университет информационных технологий, механики и оптики", "",
                 "Аспирантура (программист С, С++)", "", LocalDate.of(1993, 9, 1), LocalDate.of(1996, 7, 1));
-        Organization spbNii2 = new Organization("Санкт-Петербургский национальный исследовательский университет информационных технологий, механики и оптики",
+        Organization spbNii2 = new Organization("Санкт-Петербургский национальный исследовательский университет информационных технологий, механики и оптики", "",
                 "Инженер (программист Fortran, C)", "", LocalDate.of(1987, 9, 1), LocalDate.of(1993, 7, 1));
-        Organization mfti = new Organization("MFTI", "Заочная физико-техническая школа при МФТИ", "", LocalDate.of(1984, 9, 1),
+        Organization mfti = new Organization("MFTI", "", "Заочная физико-техническая школа при МФТИ", "", LocalDate.of(1984, 9, 1),
                 LocalDate.of(1987, 6, 1));
         add(coursera, luxofEdu, siemensAg, alcatelEdu, spbNii, spbNii2, mfti, educationPlaces);
         Section education = new OrganisationSection(educationPlaces);
