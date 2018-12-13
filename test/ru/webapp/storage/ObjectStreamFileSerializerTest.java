ec034ee545bf7dfb0eb7d048b824130ccf31583d
dancelikefish <gr.golovenko@gmail.com>
2018-12-13 17:40:37 +0300

    HW-9, ver.2 to review. - fixes

---
+++ test/ru/webapp/storage/ObjectStreamFileSerializerTest.java
@@ -0,0 +1,41 @@
+package ru.webapp.storage;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import ru.webapp.model.Resume;
+
+import java.io.*;
+import java.util.Objects;
+
+public class ObjectStreamFileSerializerTest extends AbstractStorageTest {
+    private ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("D:\\dir\\test"));
+    private ObjectInputStream ois = new ObjectInputStream(new FileInputStream("D:\\dir\\test"));
+    private SerializationStrategy ss = new ObjectStreamSerializer();
+
+    public ObjectStreamFileSerializerTest() throws IOException {
+        super(new FileStorage(new File(STORAGE_DIR.toString()), new ObjectStreamSerializer()));
+    }
+
+    @Before
+    public void serialize() {
+        try {
+            ss.serialize(R5, oos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void deserialize() {
+        Resume R6 = null;
+        try {
+            R6 = ss.deserialize(ois);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(R5.toString(), Objects.requireNonNull(R6).toString());
+    }
+
+
+}
