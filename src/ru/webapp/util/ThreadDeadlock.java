66feb9a1448d39f0180204ee183161d5e8c85fe5
dancelikefish <gr.golovenko@gmail.com>
2019-01-10 10:55:01 +0300

    HW_11, refactor ThreadDeadlock with method and lambda

--- src/ru/webapp/util/ThreadDeadlock.java
+++ src/ru/webapp/util/ThreadDeadlock.java
@@ -1,47 +1,29 @@
 package ru.webapp.util;
 
 public class ThreadDeadlock {
-    private static final Object lock1 = new Object();
-    private static final Object lock2 = new Object();
 
     public static void main(String[] args) {
-        Thread1 thread1 = new Thread1();
-        Thread2 thread2 = new Thread2();
-        thread1.start();
-        thread2.start();
+        Object lock1 = new Object();
+        Object lock2 = new Object();
+        deadlock(lock1, lock2);
+        deadlock(lock2, lock1);
     }
 
-    private static class Thread1 extends Thread {
-        public void run() {
+    private static void deadlock(Object lock1, Object lock2) {
+        new Thread(() -> {
+            System.out.println("waiting for lock 1...");
             synchronized (lock1) {
-                System.out.println("Thread 1, holding lock 1...");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Thread 1, waiting for lock 2...");
-                synchronized (lock2) {
-                    System.out.println("Thread 1, holding lock 1 & 2...");
-                }
+                System.out.println("holding lock 1...");
             }
-        }
-    }
-
-    private static class Thread2 extends Thread {
-        public void run() {
+            try {
+                Thread.sleep(45);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("waiting for lock 2...");
             synchronized (lock2) {
-                System.out.println("Thread 2, holding lock 2...");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Thread 2, waiting for lock 1...");
-                synchronized (lock1) {
-                    System.out.println("Thread 2, holding lock 1 & 2...");
-                }
+                System.out.println("holding lock 1 & 2...");
             }
-        }
+        }).start();
     }
 }
