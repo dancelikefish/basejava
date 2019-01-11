44f16610bb5c420763a15b31d7e345ced1250837
dancelikefish <gr.golovenko@gmail.com>
2019-01-11 13:21:04 +0300

    HW_12, introduce oddOrEven method

--- src/ru/webapp/util/HW12.java
+++ src/ru/webapp/util/HW12.java
@@ -1,25 +1,40 @@
 package ru.webapp.util;
 
-import java.util.*;
-import java.util.function.Consumer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class HW12 {
     public static void main(String[] args) {
-        int[] values = {1,4,6,8,9,3,5,6,8,9,1,2,3};
-        minValue(values);
+        int[] values = {9, 8, 7, 1, 1, 1, 2};
+        System.out.println(minValue(values));
+
+        List<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(3);
+        integers.add(2);
+        integers.add(1);
+        System.out.println(oddOrEven(integers));
     }
 
     private static int minValue(int[] values) {
         List<Integer> uniqueValues = new ArrayList<>();
-        Arrays.stream(values).distinct().forEach(uniqueValues::add);
-        Collections.sort(uniqueValues);
-        int start = uniqueValues.get(0);
-        uniqueValues.stream().map(value -> value)
+        Arrays.stream(values).distinct().sorted().forEach(uniqueValues::add);
+        final int[] start = {uniqueValues.get(0)};
+        IntStream.range(1, uniqueValues.size()).forEach(i -> start[0] = start[0] * 10 + uniqueValues.get(i));
+        return start[0];
+    }
 
-        for (Integer integer : uniqueValues) {
-            System.out.println(integer);
-        }
-        return 4;
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum;
+        sum = integers.stream().mapToInt(i -> i).sum();
+        System.out.println("Sum = " + sum);
+        if (sum % 2 != 0) {
+            integers.removeIf(i -> i % 2 != 0);
+        } else
+            integers.removeIf(i -> i % 2 == 0);
+        return integers;
     }
 }
-}
+
