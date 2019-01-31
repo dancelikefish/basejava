f47791d06e20fe0063088c8878bd477b1c32b115
dancelikefish <gr.golovenko@gmail.com>
2019-01-31 16:24:21 +0300

    HW_16, refactor SqlStorage - introducing jsp (list.jsp)

--- src/ru/webapp/web/ResumeServlet.java
+++ src/ru/webapp/web/ResumeServlet.java
@@ -1,7 +1,6 @@
 package ru.webapp.web;
 
 import ru.webapp.Config;
-import ru.webapp.model.Resume;
 import ru.webapp.storage.Storage;
 
 import javax.servlet.ServletException;
@@ -9,36 +8,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class ResumeServlet extends HttpServlet {
-    private Storage storage = Config.get().getStorage();
+    private Storage storage;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        storage = Config.get().getStorage();
+    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset =UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello from get servlet!" : "Hello " + name + "!");
+        request.setAttribute("resumes", storage.getAllSorted());
+        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
 
-        PrintWriter writer = response.getWriter();
-        writer.write(
-                "<div style=\"text-align: center;\">\n" +
-                        "<table border=\"2\" width=\"200\" height=\"150\">\n" +
-                        "    <tr>\n" +
-                        "        <td><strong>Uuid</strong></td>\n" +
-                        "        <td><strong>Full Name</strong></td>\n" +
-                        "    </tr>\n");
-        for (Resume resume : storage.getAllSorted()) {
-            writer.write("<tr>\n" +
-                    "        <td>" + resume.getUuid() + "</td>\n" +
-                    "        <td>" + resume.getFullName() + "</td>\n" +
-                    "    </tr>\n");
-        }
-        writer.write("</table>\n" + "</div>");
+
     }
 }
---
+++ web/WEB-INF/jsp/list.jsp
@@ -0,0 +1,29 @@
+<%@ page import="ru.webapp.model.Resume" %>
+<%@ page import="java.util.List" %>
+<%@ page import="ru.webapp.model.ContactType" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta http-equiv="Content-type" content="text/html" ; charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Список всех резюме</title>
+</head>
+<body>
+<section>
+    <table border="1" cellpadding="8" cellspacing="0">
+        <tr>
+            <th>Имя</th>
+            <th>Email</th>
+        </tr>
+        <% for (Resume resume : (List<Resume>) request.getAttribute("resumes")) {
+        %>
+        <tr>
+            <td><a href="resume?uuid=<%=resume.getUuid()%>"><%=resume.getFullName()%></a>
+            </td>
+            <td><%=resume.getContact(ContactType.MAIL)%>
+            </td>
+                <% } %>
+    </table>
+</section>
+</body>
+</html>
